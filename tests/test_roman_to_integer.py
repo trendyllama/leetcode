a69d09269c5389f17fdc7aca34961768a89a4c84
trendyllama <143849158+trendyllama@users.noreply.github.com>
2025-01-18 23:32:30 +0000

    Add initial implementation of Roman numeral converter and tests

---
+++ tests/test_roman_to_integer.py
@@ -0,0 +1,13 @@
+
+from src.individual_problems.roman_to_integer import Solution
+
+def test_roman_to_int():
+
+    assert Solution().roman_numeral_to_int("III") == 3
+    assert Solution().roman_numeral_to_int("IV") == 4
+    assert Solution().roman_numeral_to_int("IX") == 9
+    assert Solution().roman_numeral_to_int("LVIII") == 58
+    assert Solution().roman_numeral_to_int("MCMXCIV") == 1994
+    assert Solution().roman_numeral_to_int("MMXXI") == 2021
+    assert Solution().roman_numeral_to_int("MMXX") == 2020
+    assert Solution().roman_numeral_to_int("MMXIX") == 2019
