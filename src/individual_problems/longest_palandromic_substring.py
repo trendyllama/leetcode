b9b0fdda7910d6030f565f666346bd7cbf38b831
trendyllama <143849158+trendyllama@users.noreply.github.com>
2025-01-19 21:48:13 +0000

    adding two sums problem

--- src/individual_problems/longest_palandromic_substring.py
+++ src/individual_problems/longest_palandromic_substring.py
@@ -63,20 +63,20 @@ def get_largest_palandrome(input_string: str) -> str:
 
     return max(palandromes, key=len)
 
-def expand_search_after_finding_palandrome(input_string) -> str:
+def expand_search(input_string: str, start_index: int) -> str:
+    pass
 
-   pass
 
-def smart_get_largest_palandrome(input_string: str) -> str:
-  '''
-  this can be acheived with a similar backbone as the brute force method
+def smart_get_largest_palandrome(input_string: str) -> str | None:
+    '''
+    this can be acheived with a similar backbone as the brute force method
 
-  - a possible solution is once we find the smallest palandrome at index [m, n]
+    - a possible solution is once we find the smallest palandrome at index [m, n]
     we can check if [m-1, n+1] is a palandrome, [m-2, n+2], ... and so on
     when [m-i, n+i] is not a palandrome, we have found a *local* largest palandrome
-  - we can then resume our search at n+i
- 
-  '''
+    - we can then resume our search at n+i
+
+    '''
     if not input_string.isalpha() or not input_string.islower():
         raise ValueError("all characters of the input string must be letters")
 
@@ -92,15 +92,9 @@ def smart_get_largest_palandrome(input_string: str) -> str:
     if len(input_string) > 1000:
         raise ValueError("undefined for string inputs longer than 1000 chars")
 
-   palandromes = set()
+    largest_palandrome: str | None = None
 
     for idx, _ in enumerate(input_string):
-        for idx2, _ in enumerate(input_string):
-            if is_palandrome(input_string[idx:idx2]):
-              expand_search_after_finding_palandrome(input_string, start_index, end_index)
-                
+        expand_search(input_string, idx)
 
-     if len(palandromes) < 1:
-        return ""
-
-    return max(palandromes, key=len)
+    return largest_palandrome
